dfb0855296da8d50dacd6f7ae604c534913eb70e
Fabio Oshiro <fabio.oshiro@gmail.com>
2022-11-22 17:52:11 -0300

    new repo for solana adapter

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cartesi-solana
+
+Lib written in Rust to help you run a Solana program inside the Cartesi Machine
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub mod anchor_lang;
+pub mod anchor_spl;
+pub mod owner_manager;
+pub mod adapter;
+pub mod transaction;
+pub mod account_manager;
